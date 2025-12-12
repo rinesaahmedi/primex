02d7777025d7f3d031d299ba190190f76b8d0827
rinesaahmedi <149687156+rinesaahmedi@users.noreply.github.com>
2025-12-12 14:49:37 +0100

    send icon in chatbot

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -254,7 +254,7 @@ const ChatBot = () => {
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 translate-x-0.5" // Small fix to visually center the arrow
+                  className="h-5 w-5" 
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
